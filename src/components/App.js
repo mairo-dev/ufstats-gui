04c7a096d198e66848c0c1a7360f0b613236a379
Roman Maiun <roma.informal@gmail.com>
2020-02-20 23:10:43 +0200

    additional styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import StatsTable from "./StatsTable";
 import Chart from "./Chart";
-import Navigation from  "./Navigation"
+import Navigation from "./Navigation";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
         <BrowserRouter>
-            <div className="container">
-                <h3 className="m3 d-flex justify-content-center">
-                    Test project demo
-                </h3>
-                <h5 className="m3 d-flex justify-content-center">
-                    Stats table
-                </h5>
-                <Navigation/>
-                <Switch>
-                    <Route path='/' component={StatsTable} exact/>
-                    <Route path='/stats' component={StatsTable}/>
-                    <Route path='/chart' component={Chart}/>
-                </Switch>
-            </div>
+          <div className="container">
+            <Navigation/>
+            <Switch>
+              <Route path='/' component={StatsTable} exact/>
+              <Route path='/stats' component={StatsTable}/>
+              <Route path='/chart' component={Chart}/>
+            </Switch>
+          </div>
         </BrowserRouter>
     );
+  }
+
+
 }
 
 export default App;
--- src/components/StatsTable.js
+++ src/components/StatsTable.js
@@ -1,69 +1,94 @@
 import React from 'react';
 import StatsService from '../services/statsService.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Table} from 'react-bootstrap';
-import '../styles/statsTable.css'
+import {DropdownButton, Dropdown, Table} from 'react-bootstrap';
+import '../styles/statsTable.css';
+
 class StatsTable extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+
     }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  renderTableData() {
+    return StatsService.list().map((stat, index) => {
+      console.log(stat);
+      let columns = stat.map((e, i) => {
+
+        return (<td style={this.checkStyle(e)}>{e}</td>);
+      });
+      return (<tr>{columns}</tr>);
+    });
+  }
+
+  checkStyle(tdData) {
+    if (tdData === "+50") {
+      return {backgroundColor: '#006400', color: '#FFFFFF'};
+    } else if (tdData === "+25") {
+      return {backgroundColor: '#98FB98'};
+    } else if (tdData === "-25") {
+      return {backgroundColor: '#FFA07A'};
+    } else if (tdData === "-50") {
+      return {backgroundColor: '#B22222', color: '#FFFFFF'};
+    } else {
+      return {backgroundColor: '#FFFFFF'};
     }
+  }
 
-    componentWillUnmount() {
-    }
+  renderHeaders() {
+    return StatsService.headersList().map((h, i) => {
+      return (
+          <th>{h}</th>
+      );
+    });
+  }
 
-    renderTableData() {
-        return StatsService.list().map((stat, index) => {
-            console.log(stat);
-            let columns = stat.map((e, i) => {
+  _prepareOptions() {
+    return (["option 1", "option 2", "option 3"].map((e) => {
+      return <option>{e}</option>;
+    }));
+  };
 
-                return (<td style={this.checkStyle(e)}>{e}</td>);
-            });
-            return (<tr>{columns}</tr>);
-        });
-    }
-
-    checkStyle(tdData) {
-        if (tdData === "+50") {
-            return {backgroundColor: '#006400', color: '#FFFFFF'};
-        } else if (tdData === "+25") {
-            return {backgroundColor: '#98FB98'};
-        } else if (tdData === "-25") {
-            return {backgroundColor: '#FFA07A'};
-        } else if (tdData === "-50") {
-            return {backgroundColor: '#B22222',color: '#FFFFFF'};
-        } else {
-            return {backgroundColor: '#FFFFFF'};
-        }
-    }
-
-    renderHeaders() {
-        return StatsService.headersList().map((h, i) => {
-            return (
-                <th>{h}</th>
-            );
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1 id='title' class="center">UTF STATS</h1>
-                <Table bordered hover size="sm">
-                    <thead>
-                    <tr>
-                        {this.renderHeaders()}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.renderTableData()}
-                    </tbody>
-                </Table>
+  render() {
+    return (
+        <div>
+          <div className="col-sm-12 row">
+            <div className="col-sm-10 center">UTF STATS</div>
+            <div className="col-sm-2 dropdown-padding">
+              <DropdownButton id="dropdown-button"
+                              variant ="secondary"
+                              title="Select awesome period"
+                              size="sm">
+                <Dropdown.Item onClick={console.log("action")}>Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
             </div>
-        );
-    }
+          </div>
+          <div className="col-sm-12 row full-container">
+            <Table bordered hover size="sm">
+              <thead>
+              <tr>
+                {this.renderHeaders()}
+              </tr>
+              </thead>
+              <tbody>
+              {this.renderTableData()}
+              </tbody>
+            </Table>
+          </div>
+
+        </div>
+    );
+  }
 }
 
 export default StatsTable;
